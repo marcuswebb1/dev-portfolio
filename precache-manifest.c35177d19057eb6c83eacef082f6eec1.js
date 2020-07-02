6fcf4a5005e9ff17eb64d145e0eb4d9060beb886
Marcuswebb1211 <Marcuswebb1211@gmail.com>
2020-07-02 00:26:10 -0400

    Updates

---
+++ precache-manifest.c35177d19057eb6c83eacef082f6eec1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e595c4fd21b8a21c79d330662c8b965",
+    "url": "/dev-portfolio/index.html"
+  },
+  {
+    "revision": "f7dcf7a1f990c1d8b83a",
+    "url": "/dev-portfolio/static/css/main.07bae090.chunk.css"
+  },
+  {
+    "revision": "b574af7489dee9bbc099",
+    "url": "/dev-portfolio/static/js/2.0e271bf7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dev-portfolio/static/js/2.0e271bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7dcf7a1f990c1d8b83a",
+    "url": "/dev-portfolio/static/js/main.c4ad37c7.chunk.js"
+  },
+  {
+    "revision": "a24f3002b8f7ee7246a7",
+    "url": "/dev-portfolio/static/js/runtime-main.6694479f.js"
+  },
+  {
+    "revision": "327f8eedfd53da55bf4a7ea8ada983f2",
+    "url": "/dev-portfolio/static/media/grabengo.327f8eed.jpg"
+  },
+  {
+    "revision": "85677b93f2f40b68b1d76141f482deb9",
+    "url": "/dev-portfolio/static/media/prime-technologies-thumbnail.85677b93.jpg"
+  },
+  {
+    "revision": "28c9dda155b6f9be290079e9b1d31d76",
+    "url": "/dev-portfolio/static/media/resume.28c9dda1.pdf"
+  }
+]);
